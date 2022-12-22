bdc3b141208868bb7146fc1097781d37accab8a9
Taegyeong Lee <hanarotg@naver.com>
2022-12-22 15:07:32 +0900

    [del] kubernetes 카테고릴 삭제

--- _pages/about.md
+++ _pages/about.md
@@ -1,12 +1,11 @@
 ---
 layout: single
+title: "Taegyeong Lee"
 ---
-
-# Lee Taegyeong
 
 안녕하세요, 이태경입니다.
 
-Devops, Kubernetes 네이티브 개발과 React-Native를 활용한 모바일 어플리케이션 개발에 관심이 많습니다.
+React-Native를 활용한 모바일 어플리케이션 개발에 관심이 많습니다.
 
 - Email : hanarotg@naver.com
 - Linkedin : [https://www.linkedin.com/in/hanarotg/](https://www.linkedin.com/in/hanarotg/)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -70,4 +70,43 @@ var store = [{
         "tags": ["stylehero"],
         "url": "/business/project-stylehero-story-3/",
         "teaser": null
-      }]
+      },{
+    "title": "Taegyeong Lee",
+    "excerpt":"안녕하세요, 이태경입니다.   React-Native를 활용한 모바일 어플리케이션 개발에 관심이 많습니다.      Email : hanarotg@naver.com   Linkedin : https://www.linkedin.com/in/hanarotg/   Github : https://github.com/hanarotg   Blog : https://hanarotg.github.io   CV   준비중입니다.  ","url": "http://localhost:4000/_pages/about/"
+  },{
+    "title": "Posts by Categories",
+    "excerpt":" ","url": "http://localhost:4000/categories/"
+  },{
+    "title": null,
+    "excerpt":"","url": "http://localhost:4000/"
+  },{
+    "title": "Kubernetes",
+    "excerpt":" ","url": "http://localhost:4000/tags/kubernetes/"
+  },{
+    "title": null,
+    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "http://localhost:4000/assets/js/lunr/lunr-en.js"
+  },{
+    "title": null,
+    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "http://localhost:4000/assets/js/lunr/lunr-gr.js"
+  },{
+    "title": null,
+    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "http://localhost:4000/assets/js/lunr/lunr-store.js"
+  },{
+    "title": "React-Native",
+    "excerpt":"","url": "http://localhost:4000/tags/react-native/"
+  },{
+    "title": null,
+    "excerpt":"{% for post in site.posts %} {{ site.url }}{{ post.url }} {% if post.lastmod == null %} {{ post.date | date_to_xmlschema }} {% else %} {{ post.lastmod | date_to_xmlschema }} {% endif %} {% if post.sitemap.changefreq == null %} weekly {% else %} {{ post.sitemap.changefreq }} {% endif %} {%...","url": "http://localhost:4000/sitemap.xml"
+  },{
+    "title": "Posts by Tags",
+    "excerpt":"","url": "http://localhost:4000/tags/"
+  },{
+    "title": null,
+    "excerpt":"","url": "http://localhost:4000/page2/"
+  },{
+    "title": null,
+    "excerpt":"Sitemap: {{ \"sitemap.xml\" | absolute_url }} ","url": "http://localhost:4000/robots.txt"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "http://localhost:4000/feed.xml"
+  }]
