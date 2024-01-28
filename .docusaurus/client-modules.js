1b71fd850ddced155300aa62262337af80fcd31e
Taegyeong Lee <hanarotg@naver.com>
2024-01-28 15:41:46 +0900

    Deploy website - based on 0f62e52bc99392fd383b2d8f48b2d72352922488

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/taegyeonglee/Documents/GitHub/hanarotg.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/taegyeonglee/Documents/GitHub/hanarotg.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/taegyeonglee/Documents/GitHub/hanarotg.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/taegyeonglee/Documents/GitHub/hanarotg.github.io/src/css/custom.css"),
-];
