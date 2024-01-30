222250138e4aed8e3f9dbe7f551fc0881dce4f60
Taegyeong Lee <hanarotg@naver.com>
2024-01-30 16:50:12 +0900

    update .

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,12 +1,19 @@
-import React from "react";
-import DocItem from "@theme-original/DocItem";
-import Comment from "../../components/HomepageFeatures/Comment";
-
-export default function DocItemWrapper(props) {
+import React from 'react';
+import {HtmlClassNameProvider} from '@docusaurus/theme-common';
+import {DocProvider} from '@docusaurus/theme-common/internal';
+import DocItemMetadata from '@theme/DocItem/Metadata';
+import DocItemLayout from '@theme/DocItem/Layout';
+export default function DocItem(props) {
+  const docHtmlClassName = `docs-doc-id-${props.content.metadata.id}`;
+  const MDXComponent = props.content;
   return (
-    <>
-      <DocItem {...props} />
-      <Comment />
-    </>
+    <DocProvider content={props.content}>
+      <HtmlClassNameProvider className={docHtmlClassName}>
+        <DocItemMetadata />
+        <DocItemLayout>
+          <MDXComponent />
+        </DocItemLayout>
+      </HtmlClassNameProvider>
+    </DocProvider>
   );
 }
